ec186c793e58044c619a8861c2acbb061fad9526
Aditya Mhamunkar <me@adityamhamunkar.com>
2018-04-28 21:45:22 -0400

    feat: inital server setup done
    - folder structure
    - primary libraries imported
    - server running on port 3000

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
+var jwt = require('jsonwebtoken');
+
+
+const app = express();
+
+
+
+
+//const api_route = require('./routes/api_route.js');
+
+// connect to mongodb
+// mongoose.connect('mongodb://localhost:27017/expense_tracker_users');
+// mongoose.connection.on('connected', ()=>{
+// 	console.log('connected to database MongoDB');
+// });
+// mongoose.connection.on('error', (err)=>{
+// 	if(err){
+// 		console.log('Error in database connection: '+ err);
+// 	}
+// });
+
+
+// Middleware - cors
+app.use(cors());
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+//app.use('/api', api_route)
+
+// testing server
+app.get('/', function(req, res){
+	res.send('<h1> Hello there, I am working! </h1>');
+});
+
+
+
+
+app.listen(3000, function(){
+	console.log('Server running on port 3000...');
+})
